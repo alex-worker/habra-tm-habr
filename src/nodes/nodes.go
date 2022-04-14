56081aada32e906c51fe827774b364d249e7b847
alex-worker <alex@amberskynet.org>
2022-04-14 14:31:55 +0300

    some refactoring

--- src/nodes/nodes.go
+++ src/nodes/nodes.go
@@ -7,22 +7,6 @@ import (
 	"io"
 )
 
-//func AddSomeTM(reader *strings.Reader) (string, error) {
-//	doc, err := html.Parse(reader)
-//	if err != nil {
-//		return "", err
-//	}
-//
-//	nodeAddTM(doc, replacer.DoSomeTM)
-//
-//	var buffer = new(bytes.Buffer)
-//	err = html.Render(buffer, doc)
-//	if err != nil {
-//		return "", err
-//	}
-//	return string(buffer.Bytes()), nil
-//}
-
 func BytesToHTML(reader io.Reader) (*html.Node, error) {
 	doc, err := html.Parse(reader)
 	if err != nil {
@@ -40,7 +24,7 @@ func HTMLToBytes(doc *html.Node) ([]byte, error) {
 	return buffer.Bytes(), nil
 }
 
-func NodeAddTM(node *html.Node, replacerFn func(string) string) {
+func Update(node *html.Node, updateFn func(string) string) {
 	if node.Type == html.TextNode {
 		parentDataName := node.Parent.Data
 
@@ -55,10 +39,10 @@ func NodeAddTM(node *html.Node, replacerFn func(string) string) {
 
 		nodeIsInArray := slices.Contains(proceedNode, parentDataName)
 		if nodeIsInArray {
-			node.Data = replacerFn(node.Data)
+			node.Data = updateFn(node.Data)
 		}
 	}
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
-		NodeAddTM(child, replacerFn)
+		Update(child, updateFn)
 	}
 }
