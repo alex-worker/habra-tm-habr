239206d33a1128d80a3b8068a472a04a4de3de00
alex-worker <alex@amberskynet.org>
2022-04-15 11:06:56 +0300

    testing update

--- src/handler/handler_test.go
+++ src/handler/handler_test.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	//. "github.com/onsi/ginkgo/v2"
+	//. "github.com/onsi/gomega"
 	"io"
 	"log"
 	"net/http"
@@ -11,22 +11,20 @@ import (
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Handler Suite")
-}
+//func TestHandler(t *testing.T) {
+//	RegisterFailHandler(Fail)
+//	RunSpecs(t, "Handler Suite")
+//}
 
 func TestProxyHandler_ServeHTTP(t *testing.T) {
 	fixture := `<html><head><title>Hello</title></head><body><h1>Приве!т</h1></body></html>`
 	expected := `<html><head><title>Hello</title></head><body><h1>Приве™!т</h1></body></html>`
 
-	var (
-	//mut sync.Mutex
-	//headers http.Header
-	)
+	headersChan := make(chan map[string][]string)
+	bodyChan := make(chan []byte)
 
 	fixtureServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//log.Println(r.Header)
+		headersChan <- r.Header
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte(fixture))
 		if err != nil {
@@ -52,20 +50,25 @@ func TestProxyHandler_ServeHTTP(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	resp, err := http.Get(srv.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
+	go func() {
+		resp, err := http.Get(srv.URL)
+		if err != nil {
+			t.Error(err)
+		}
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		bodyChan <- respBody
+	}()
 
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+	respHeaders := <-headersChan
+	respBody := <-bodyChan
+
+	log.Println("resp.Header:", respHeaders)
 
 	actual := string(respBody)
 	if actual != expected {
 		t.Errorf("Actual: %s expected: %s", actual, expected)
 	}
-
-	log.Println("resp.Header:", resp.Header)
 }
