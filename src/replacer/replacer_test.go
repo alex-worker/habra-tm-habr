824f5a8217c36e6dba55423aa5fab71a38aa4290
alex-worker <alex-worker@email>
2022-04-15 11:06:56 +0300

    testing update

--- src/replacer/replacer_test.go
+++ src/replacer/replacer_test.go
@@ -1,36 +1,34 @@
 package replacer
 
-import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"testing"
-)
+import "testing"
 
-func TestReplacer(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Replacer Suite")
+func Test_DoSomeTM(t *testing.T) {
+	testStr := "Наверное, в любых проектах есть необходимость использования различных секретных данных - строки подключения к БД, АПИ-ключи внешних сервисов и т.д.."
+	actual := DoSomeTM(testStr)
+	expected := "Наверное, в любых™ проектах есть необходимость использования различных секретных данных - строки подключения к БД, АПИ-ключи™ внешних сервисов и т.д.."
+	if actual != expected {
+		t.Errorf("Actual %v but expected %v", actual, expected)
+	}
 }
 
-var _ = Describe("DoSomeTM", func() {
-	It("Worked", func() {
-		testStr := "Наверное, в любых проектах есть необходимость использования различных секретных данных - строки подключения к БД, АПИ-ключи внешних сервисов и т.д.."
-		actual := DoSomeTM(testStr)
-		expected := "Наверное, в любых™ проектах есть необходимость использования различных секретных данных - строки подключения к БД, АПИ-ключи™ внешних сервисов и т.д.."
-		Expect(actual).To(Equal(expected))
-	})
-})
-
-var _ = Describe("doReplace", func() {
-	It("it must be worked", func() {
-		testStr := "Наверное "
-		actual := doReplace(testStr)
-		expected := "Наверное™ "
-		Expect(actual).To(Equal(expected))
-	})
-	It("it must be worked too", func() {
-		testStr := "Наверное"
-		actual := doReplace(testStr)
-		expected := "Наверно™е"
-		Expect(actual).To(Equal(expected))
-	})
-})
+func Test_doReplace(t *testing.T) {
+	tests := []struct {
+		inputStr string
+		expected string
+	}{
+		{
+			inputStr: "Наверное ",
+			expected: "Наверное™ ",
+		},
+		{
+			inputStr: "Наверное",
+			expected: "Наверно™е",
+		},
+	}
+	for _, test := range tests {
+		actual := doReplace(test.inputStr)
+		if actual != test.expected {
+			t.Errorf("Actual %v but expected %v", actual, test.expected)
+		}
+	}
+}
