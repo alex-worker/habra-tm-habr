b62d5638ea152384e97b98ae02161405c959ddf1
alex-worker <alex-worker@email>
2022-04-16 16:02:50 +0300

    add handler class

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -8,7 +8,6 @@ import (
 )
 
 type ProxyHandler struct {
-	//SiteAddress *url.URL
 	Processor *RequestProcessor
 }
 
@@ -23,23 +22,8 @@ func (p *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Addr: ", r.RemoteAddr, "Method:", r.Method, "URL: ", r.URL.String())
 
-	if r.Method != http.MethodGet {
-		log.Printf("Method not supported %s\n", r.Method)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	//cli := http.Client{}
-
-	// set req Host, URL and Request URI to forward a request to the origin server
-	//r.Host = p.SiteAddress.Host
-	//r.URL.Host = p.SiteAddress.Host
-	//r.URL.Scheme = p.SiteAddress.Scheme
-	//r.RequestURI = ""
-
 	resp, err := p.Processor.processRequest(r)
-	//delHeaders(r.Header)
-	//resp, err := cli.Do(r)
+
 	if err != nil {
 		log.Printf(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
