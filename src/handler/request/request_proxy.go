c4aa8ba774b80675d6ec4f1ff20e5c99f3493559
alex-worker <alex@amberskynet.org>
2022-04-17 11:28:21 +0300

    remove comment

--- src/handler/request/request_proxy.go
+++ src/handler/request/request_proxy.go
@@ -24,26 +24,3 @@ func NewRequestProxy(proxyUrl *url.URL) ProcessRequest {
 		return cli.Do(r)
 	}
 }
-
-//type Processor struct {
-//	SiteAddress *url.URL
-//}
-
-//func (h *Processor) Request(r *http.Request) (*http.Response, error) {
-//
-//	if r.Method != http.MethodGet {
-//		msg := fmt.Sprintf("Method not supported %s\n", r.Method)
-//		return nil, errors.New(msg)
-//	}
-//
-//	cli := http.Client{}
-//
-//	// set req Host, URL and Request URI to forward a request to the origin server
-//	r.Host = h.SiteAddress.Host
-//	r.URL.Host = h.SiteAddress.Host
-//	r.URL.Scheme = h.SiteAddress.Scheme
-//	r.RequestURI = ""
-//
-//	headers.DelHeaders(r.Header)
-//	return cli.Do(r)
-//}
